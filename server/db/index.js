f6e65259bd4d4340864af91d268d567633f9ffa7
Ivan Yavorksy <02ivan0971@gmail.com>
2023-06-01 16:02:43 +0300

    Add env variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/logistics", { useNewUrlParser: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  // .connect("mongodb://127.0.0.1:27017/logistics", { useNewUrlParser: true })
   .catch((e) => {
     console.error("Connection error", e.message);
   });
